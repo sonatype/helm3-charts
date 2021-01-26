185af6e10d053b288722653afd8f76c2d12c13f2
John Flinchbaugh <jflinchbaugh@sonatype.com>
2021-01-26 00:36:38 -0500

    headers

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # Copyright (c) 2019-present Sonatype, Inc. All rights reserved.
+# Includes the third-party code listed at http://links.sonatype.com/products/nexus/attributions.
 # "Sonatype" is a trademark of Sonatype, Inc.
 #
 
