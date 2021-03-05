af46f1bddb7568d919b552c426df7fe8d1c34d31
John Flinchbaugh <jflinchbaugh@sonatype.com>
2021-03-04 23:02:02 -0500

    assert.sh lib + more testing

--- test/run.sh
+++ test/run.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# https://github.com/torokmark/assert.sh
+. test/assert.sh
+
 set -x -e
 
 # watch everything: watch kubectl get all --all-nameespaces
 
 mkdir -p test/output
+rm -f test/output/*
 
 function shutdown {
-    minikube logs -p helm-test > test/output/minikube.log
+    #minikube logs -p helm-test 2>&1 > test/output/minikube.log
     minikube stop -p helm-test
     minikube delete -p helm-test
 }
@@ -22,12 +26,16 @@ echo '--------------------'
 echo 'Nexus Repository Manager'
 helm install nexus-repository-manager \
      $BASEDIR/charts/nexus-repository-manager
-helm list | grep 3.29.0 || echo "FAIL: nxrm version"
+helm_installs=$(helm list)
+assert_contain "$helm_installs" 3.29.0
 
 kubectl wait \
         --for=condition=available \
         --timeout=5m \
         deployments/nexus-repository-manager
+
+helm test nexus-repository-manager \
+     > $BASEDIR/test/output/test-nexus-repostiory-manager.log
 
 POD_NAME=$(kubectl get pods \
                    -l "app.kubernetes.io/name=nexus-repository-manager,app.kubernetes.io/instance=nexus-repository-manager" \
@@ -38,7 +46,7 @@ PF_PID=$!
 
 sleep 4
 
-curl -v -L http://localhost:8081/ > test/output/nxrm.html
+curl -L http://localhost:8081/ > test/output/nexus-repository-manager.html 2>&1
 
 kill -INT $PF_PID
 
@@ -53,12 +61,16 @@ echo '--------------------'
 echo 'Nexus IQ Server'
 helm install nexus-iq \
      $BASEDIR/charts/nexus-iq
-helm list | grep 1.105.0 || echo "FAIL: iq version"
+helm_installs=$(helm list)
+assert_contain "$helm_installs" 1.105.0 
 
 kubectl wait \
         --for=condition=available \
         --timeout=5m \
         deployments/nexus-iq-nexus-iq-server
+
+helm test nexus-iq \
+    > $BASEDIR/test/output/test-nexus-iq.log
 
 POD_NAME=$(kubectl get pods \
     -l "app.kubernetes.io/name=nexus-iq-server,app.kubernetes.io/instance=nexus-iq" \
@@ -68,7 +80,7 @@ PF_PID=$!
 
 sleep 4
 
-curl -v -L  http://localhost:8070/ > test/output/iq.html
+curl -L  http://localhost:8070/ > test/output/nexus-iq.html
 
 kill -INT $PF_PID
 
